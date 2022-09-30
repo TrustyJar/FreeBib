0d4ef3a8d39e1883fbd8e275c18646f1c6a7ce81
Cyrus <cyrus.naficy@gmail.com>
2022-09-30 16:52:37 -0400

    cipher

---
+++ src/server/public/js/setCookie.js
@@ -0,0 +1,11 @@
+
+/*
+This function will set a cookie inside of the web browser. It will
+allow you to create a cookie and run it in the background.
+*/
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
